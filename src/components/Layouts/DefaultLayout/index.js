8f8280f779798531d58f0acce74bb0d72c206094
Nguyen <trungnguyenwork123.com>
2024-08-05 19:58:15 +0700

    build basic layout

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,12 +1,18 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layouts/components/Header';
-import Sidebar from '~/components/Layouts/components/SideBar';
+import Sidebar from '~/components/Layouts/components/Sidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <Sidebar />
-            {children}
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
+            </div>
         </div>
     );
 }
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <h1>Heading</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
